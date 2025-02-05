d3523cd0883fdae6828eccddb3f16c8dc792a72d
Amaan1911 <163126376+Amaan1911@users.noreply.github.com>
2025-02-05 20:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	let result = ""
+	if(s.length==0){
+		return s;
+	}
+		
+	for(let i =0; i<s.length;i++){
+		result+=s[i];
+		if(s[i]==" ")
+			break;
+	}
+	return result
+	
 }
 
 // Do not change the code below
